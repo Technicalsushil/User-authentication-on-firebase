bf7cddf1206c90e97c37a3efcae1c66c90d0d346
Oad786 <36334253+Oad786@users.noreply.github.com>
2021-03-11 15:39:16 +0530

    User authentication on firebase

---
+++ app/src/main/java/com/example/firebaseregister/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.firebaseregister;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    EditText email,password;
+    Button submit;
+    FirebaseAuth auth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        email=findViewById(R.id.email);
+        password=findViewById(R.id.password);
+        submit=findViewById(R.id.submit);
+        auth=FirebaseAuth.getInstance();
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register();
+            }
+        });
+         }
+
+    private void register() {
+        auth.createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(getApplicationContext(),"Registration Successfully",Toast.LENGTH_SHORT).show();
+                            Intent i=new Intent(getApplicationContext(),Login.class);
+                            startActivity(i);
+                        }
+                        else
+                        {
+                            Toast.makeText(getApplicationContext(),"Registration Failed",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
